53508609cc2276dd2584917281cae1f202cc58a4
Alejandro Barrios <alejandro@aleate.com>
2021-04-18 22:02:16 -0500

    feat(dark): add little dark mode to web theme

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@
 import './App.css';
 import Header from './components/Header';
 import Characters from './components/Characters';
+import ThemeContext from './context/ThemeContext';
+import { useState } from 'react'
+
+
+
 
 function App() {
+  const [theme, setTheme] = useState('light');
   return (
-    <div className="App">
-      <Header/>
-      <Characters></Characters>
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={"App " + theme }>
+        <Header/>
+        <Characters></Characters>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
+import { useContext } from 'react';
+import './../App.css';
+import ThemeContext from './../context/ThemeContext';
 
 const Header = () => {
-
-  const [darkMode, setDarkMode] = useState(false); 
-
+  let { theme, setTheme } = useContext(ThemeContext);
   const handleClick = () => {
-    setDarkMode(!darkMode)
+    theme = theme === 'dark' ? 'light' : 'dark';
+    console.log(theme, 'aqui tema');
+    setTheme(theme);
   }
 
   return (
     <div className="Header">
-      <h1>ReactHooks</h1>
-      <button type="button" onClick={handleClick}>{darkMode ? 'Dark Mode' : 'Light Mode'}</button>
-      <button type="button" onClick={() => {setDarkMode(!darkMode) }}>{darkMode ? 'Dark Mode' : 'Light Mode'}</button>
+      <h1 className="title">ReactHooks</h1>
+      <button type="button" onClick={handleClick}>{theme === 'dark' ? 'Light Mode' : 'Dark Mode'}</button>
+      {/* <button type="button" onClick={() => {setTheme(!darkMode) }}>{theme == 'dark' ? 'Dark Mode' : 'Light Mode'}</button> */}
     </div>
   );
 }

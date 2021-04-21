976f80dcc6e9a9aebbaff6b046750a5dc87eecf2
Alejandro Barrios <alejandro@aleate.com>
2021-04-21 14:14:07 -0500

    feat(favorites): add character to favorites

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,8 +1,30 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useReducer } from 'react'
 import Card from './Card';
-import './../stylesheets/characters.css'
+import './../stylesheets/characters.css';
+
+const initialState = {
+  favorites: []
+}
+
+const favoriteReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TO_FAVORITES':
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload]
+      };
+    default:
+      return state;
+  }
+}
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
+
+  const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+
+  const handleClick = favorite => {
+    dispatch({ type: 'ADD_TO_FAVORITES', payload: favorite });
+  }
 
   const getCharacters = async () => {
     const response = await fetch('https://rickandmortyapi.com/api/character/');
@@ -15,14 +37,20 @@ const Characters = () => {
   }, []);
 
   const goToDetails = (character) => {
-    console.log('jajajaja', character);
+
   }
 
   return (
     <div className="characters">
+      {favorites.favorites.map(favorite => (
+        <div onClick={() => goToDetails(favorite)} key={favorite.id}>
+          <Card character={favorite}></Card>
+        </div>
+      ))}
       {characters.map(character => (
         <div onClick={() => goToDetails(character)} key={character.id}>
           <Card character={character}></Card>
+          <button type="button" onClick={() => { handleClick(character) }}>Add Favorite </button>
         </div>
       ))}
     </div>
